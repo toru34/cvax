ed580e6a9763bc863bc3f7f90fb548f844843bb2
Toru Fujino <toru.fb34@gmail.com>
2021-02-27 15:32:59 +0900

    add config

---
+++ cvax/config.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+
+BASE_DIR = Path.home() / '.cvax'
+
+DATASET_DIR = BASE_DIR / 'datasets'
+PRETRAINED_DIR = BASE_DIR / 'pretrained'
--- cvax/datasets/imagenette.py
+++ cvax/datasets/imagenette.py
@@ -1,30 +1,35 @@
+from typing import Union, Literal
 from pathlib import Path
 
 import numpy as np
 from PIL import Image
 
+from cvax import config
+
 class Imagenette:
+
     def __init__(self,
-        split, # TODO: Literal['train', 'val', 'test']
-        image_size, # TODO: Union[int, Tuple[int, int]]
-        ): # TODO: use Literal for split
+        split: Literal['train', 'val', 'test'],
+        image_size: Union[int, tuple[int, int]],
+        ):
         """
         """
+
         self.image_paths = []
         self.labels = []
 
-        self.image_size = image_size # TODO:
+        self.image_size = (image_size, image_size) if type(image_size) == int else image_size
 
         # TODO: Download dataset
 
-        data_dir = Path(f"/Users/toru34/.cvax/imagenette2/{split}") # TODO
+        data_dir = config.DATASET_DIR / f"imagenette2/{split}"
 
         self._class_ids = sorted([p.stem for p in data_dir.glob('n0*')])
         
         for _class_id in self._class_ids:
             _class_dir = data_dir / _class_id
 
-            _image_paths = sorted(_class_dir.glob('*.JPEG'))
+            _image_paths = sorted(_class_dir.glob("*.JPEG"))
 
             self.image_paths += _image_paths
             self.labels += len(_image_paths) * [_class_id]
