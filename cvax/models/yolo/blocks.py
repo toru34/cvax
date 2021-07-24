7a64f94f7b18a77372cd4f454fbbf01ddd8f8926
Toru Fujino <toru.fb34@gmail.com>
2021-07-24 17:01:32 +0900

    fix

--- cvax/models/yolo/blocks.py
+++ cvax/models/yolo/blocks.py
@@ -3,6 +3,7 @@ import jax.nn as nn
 from nmax import Module
 from cvax.modules import Conv2d, BatchNorm2d
 
+
 class YOLOConvBlock(Module):
 
     conv: Module
@@ -35,5 +36,32 @@ class YOLOConvBlock(Module):
         
         if self.activation:
             x = nn.leaky_relu(x, negative_slope=0.1) # TODO: add activation option
+
+        return x
+
+
+class DarknetResBlock(Module):
+
+    conv_in: Module
+    conv_out: Module
+
+    def __init__(self,
+        key,
+        in_channels: int,
+        ):
+
+        keys = jax.random.split(key)
+
+        self.conv_in = YOLOConvBlock(keys[0], kernel_shape=(in_channels, in_channels, 1, 1))
+        self.conv_out = YOLOConvBlock(keys[1], kernel_shape=(in_channels, in_channels, 3, 3))
+
+    def forward(self, x):
+
+        residual = x
+
+        x = self.conv_in(x)
+        x = self.conv_out(x)
+
+        x += residual
 
         return x
